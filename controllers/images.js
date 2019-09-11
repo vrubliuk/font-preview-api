7292e90f1eac6ae163c86552b8c1f62af158eae0
Valentyn Rubliuk <vrubliuk@gmail.com>
2019-09-11 14:49:38 +0300

    Remove the images package

--- controllers/images.js
+++ controllers/images.js
@@ -1,60 +1,50 @@
 const fs = require("fs");
 const text2png = require("text2png");
-const images = require("images");
 const uniqId = require("uniqid");
 const isDecimal = require("validator/lib/isDecimal");
 const fonts = require("../fonts");
 
 exports.getImage = async (req, res, next) => {
-  let { fontSize, width, handwritingStyleId, text } = req.query;
+  let { fontSize, handwritingStyleId, text } = req.query;
 
   if (!fontSize) return next(new Error("Font size is required"));
   if (!isDecimal(fontSize)) return next(new Error("Font size should be a decimal"));
-  if (!width) return next(new Error("Width is required"));
-  if (!isDecimal(width)) return next(new Error("Width should be a decimal"));
   if (!handwritingStyleId) return next(new Error("Handwriting style is required"));
   if (!text.trim()) return next(new Error("Text is required"));
 
   fontSize = Number(fontSize.trim());
-  width = Number(width.trim());
   handwritingStyleId = Number(handwritingStyleId);
-  text = text.trim();
+  text = text
+    .trim()
+    .split("\n")
+    .map(row => row.padStart(row.length + Math.floor(Math.random() * 6)))
+    .join("\n");
 
-  const textRows = text.split("\n").filter(row => row !== "");
-  
-  let font = fonts.find(font => font.id === handwritingStyleId);  
+  let font = fonts.find(font => font.id === handwritingStyleId);
   if (!font) return next(new Error("Font is not found"));
 
-  const image = images(width, (fontSize + 5) * textRows.length);
-
   try {
-    textRows.forEach((row, i) => {
-      const midImagePath = `${uniqId()}.png`;
-      fs.writeFileSync(
-        midImagePath,
-        text2png(row, {
-          font: `${fontSize}px '${font.label}'`,
-          localFontPath: `fonts/files/${font.fileName}`,
-          localFontName: `${font.label}`
-        })
-      );
-      image.draw(images(midImagePath), 5 + 15 * Math.random(), 5 + (fontSize + 5) * i);
-      fs.unlink(midImagePath, err => {
+    const imagePath = `${uniqId()}.png`;
+    fs.writeFile(
+      imagePath,
+      text2png(text, {
+        font: `${fontSize}px '${font.label}'`,
+        localFontPath: `fonts/files/${font.fileName}`,
+        localFontName: `${font.label}`,
+        padding: 20,
+        color: "teal",
+        backgroundColor: "linen"
+      }),
+      err => {
         if (err) throw err;
-      });
-    });
-
-    const finalImagePath = `final-${uniqId()}.png`;
-    image.save(finalImagePath, {
-      quality: 100
-    });
-
-    res.download(finalImagePath, downloadError => {
-      if (downloadError) throw downloadError;
-      fs.unlink(finalImagePath, deletionError => {
-        if (deletionError) throw deletionError;
-      });
-    });
+        res.download(imagePath, err => {
+          if (err) throw err;
+          fs.unlink(imagePath, err => {
+            if (err) throw err;
+          });
+        });
+      }
+    );
   } catch (err) {
     next(err);
   }
